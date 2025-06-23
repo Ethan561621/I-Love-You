00cc21ace77f79f6c18dd03a32b6e65ffae0d31b
Ethan561621 <baguiogonzaless@gmail.com>
2025-06-23 21:27:49 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,47 @@
+const CACHE_NAME = 'i-love-you-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('love-cache').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        'manifest.json',
-        'pwa-icon.png'
-      ]);
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Pre-caching offline files');
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[Service Worker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Files
+self.addEventListener('fetch', (event) => {
+  console.log('[Service Worker] Fetch', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
